3d3fefa9bcf5570477770a67e4e7180772025e4b
Jeff Simons Decena <jeff.decena@yahoo.com>
2016-06-12 15:40:41 +0800

    Added the foreign key to user

--- src/database/seeds/PageTableSeeder.php
+++ src/database/seeds/PageTableSeeder.php
@@ -14,6 +14,7 @@ class PageTableSeeder extends Seeder
     {
         Page::create([
             'title'         => 'About us',
+            'user_id' 		=> 1,
             'slug'         	=> 'about-us',
             'content'       => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?',
             'status' 		=> 1
--- src/database/seeds/PostTableSeeder.php
+++ src/database/seeds/PostTableSeeder.php
@@ -14,6 +14,7 @@ class PostTableSeeder extends Seeder
     {
         Post::create([
             'title'         => 'Hello world',
+            'user_id' 		=> 1,
             'slug'         	=> 'hello-world',
             'content'       => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?',
             'status' 		=> 1
