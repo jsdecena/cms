cd80744ebd6eab128966ebad006851e81ed9104c
Jeff Simons Decena <jeff.decena@yahoo.com>
2019-12-15 12:38:16 +0800

    Make the description of category nullable
    Signed-off-by: Jeff Simons Decena <jeff.decena@yahoo.com>

--- src/database/migrations/2016_06_12_061333_create_category_table.php
+++ src/database/migrations/2016_06_12_061333_create_category_table.php
@@ -16,7 +16,7 @@ class CreateCategoryTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('slug');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->integer('status')->default(0);
             $table->timestamps();
         });
